204d27fab7870c1cc46b47fdcf2a84706beee0cf
thiago-deve-cyber <thiagof@tutamail.com>
2025-01-31 15:35:32 -0500

    defining the attributes of the vpnhelp class

---
+++ src/vpnhelp.py
@@ -0,0 +1,37 @@
+import os
+from src.filehelp import VpnHelp
+
+
+class VpnHelp:
+	def __init__(self):
+		self.is_active = False
+		self.auth_file = None
+		self.config_file = False
+
+
+	def __set_mac_address(self):
+		pass
+
+
+	def __finish_process(self):
+		pass
+
+
+	def start_vpn(self):
+		pass
+
+
+	def get_network_interfaces(self):
+		pass
+
+
+	def check_internet_connection(self):
+		pass
+
+
+	def choose_random_configuration(self):
+		pass
+
+
+	def restart_default(self):
+		pass
