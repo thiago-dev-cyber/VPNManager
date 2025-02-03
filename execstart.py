081b429c2197e2495f04ecff22443c87f0db5d8e
thiago-dev-cyber <thiagof@tutamail.com>
2025-02-02 23:57:41 -0300

    feat: Scripts for startup and shutdown.

--- execstart.py
+++ execstart.py
@@ -3,14 +3,47 @@ import sys
 
 
 CURRENTDIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG_DIR = '/etc/vpnmmanager'
+SERVERS_DIR = os.path.join(CONFIG_DIR, 'servers')
+AUTH_FILE = '/etc/vpnmmanager/auth.txt'
+PID_FILE = '/var/run/vpnmmanager.pid'
 
 sys.path.append(CURRENTDIR)
 os.environ['ROOT'] = CURRENTDIR
 
 
-from src.vpnhelp import VpnHelp
+try:
+	from src.vpnhelp import VpnHelp
 
-VPN = VpnHelp()
+except ImportError as e:
+	print(f"Error: {e}")
+	sys.exit(1)
 
 
-print(VPN.start('auth.txt','nl-free-75.protonvpn.udp.ovpn'))
+def main() -> None:
+	try: 
+		vpn = VpnHelp()
+
+		vpn.stop()
+
+		server_file = vpn._get_random_server(SERVERS_DIR)
+
+		process = vpn.start(AUTH_FILE, server_file)
+
+		if process and process.pid:
+			with open(PID_FILE, 'w') as f:
+				f.write(str(process.pid))
+			print(f"Server start with PID: {process.pid}")
+
+		else:
+			print(f"Fail")
+			sys.exit(1)
+
+	except Exception as err:
+		print(f"Error {err}")
+		sys.exit(1)
+
+
+if __name__ == "__main__":
+	main()
+
